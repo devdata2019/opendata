d36c0682689fe3bc7851a7410077a28fe116b9bd
ejhb <bonurajosh13@gmail.com>
2020-01-20 17:24:06 +0100

    hihi

--- SQL/SCRIPT HIHI.sql
+++ SQL/SCRIPT HIHI.sql
@@ -1,43 +1,14 @@
-USE opendata;
-SELECT siret , activitePrincipaleEtablissement, LIB_NAP15
-FROM etab
-INNER join nap1973_1993 ON etab.activitePrincipaleEtablissement = nap1973_1993.NAP600
-WHERE codePostalEtablissement LIKE '130%'
-
-UNION
-
-SELECT siret , activitePrincipaleEtablissement, Libellé
-FROM etab
-INNER join naf1993niveaux ON etab.activitePrincipaleEtablissement = naf1993niveaux.N_700
-INNER join naf1993sections ON naf1993niveaux.N_17 = naf1993sections.code
-WHERE codePostalEtablissement LIKE '130%'
-
-UNION
-
-SELECT siret , activitePrincipaleEtablissement, Libellé
-FROM etab
-INNER join naf2003niveaux ON etab.activitePrincipaleEtablissement = naf2003niveaux.N_700
-INNER join naf2003sections ON naf2003niveaux.N_17 = naf2003sections.code 
-WHERE codePostalEtablissement LIKE '130%'
-
-UNION
-
-SELECT siret , activitePrincipaleEtablissement, Libellé
-FROM etab
-INNER join naf2008niveaux ON etab.activitePrincipaleEtablissement = naf2008niveaux.NIV5
-INNER join naf2008sections ON naf2008niveaux.NIV1 = naf2008sections.code
-WHERE codePostalEtablissement LIKE '130%'
-; 
-
-
-----------------------------------------------------------------------------------------------------------------
-
-
 USE opendata ;
-ALTER TABLE etablissement_marseille
-ADD INDEX idx_codeAct (activitePrincipaleEtablissement) ;
+
+ALTER TABLE etablissement_marseille  
+
+ADD INDEX idx_codeAct (activitePrincipaleEtablissement); 
+
+DROP TABLE IF EXISTS alias_etab_marseille;
+
 CREATE TABLE alias_etab_marseille AS 
-SELECT siret , activitePrincipaleEtablissement, LIB_NAP15
+
+SELECT siret , activitePrincipaleEtablissement, LIB_NAP600
 FROM etablissement_marseille
 INNER join nap1973_1993 ON etablissement_marseille.activitePrincipaleEtablissement = nap1973_1993.NAP600
 
@@ -61,6 +32,3 @@ SELECT siret , activitePrincipaleEtablissement, Libellé
 FROM etablissement_marseille
 INNER join naf2008niveaux ON etablissement_marseille.activitePrincipaleEtablissement = naf2008niveaux.NIV5
 INNER join naf2008sections ON naf2008niveaux.NIV1 = naf2008sections.code ; 
-INNER join naf2008sections ON naf2008niveaux.NIV1 = naf2008sections.code 
-; 
-
--- SQL/requete_man.py
+++ SQL/requete_man.py
@@ -6,6 +6,17 @@ Created on Tue Jan 14 10:07:02 2020
 @author: utilisateur
 """
 
+# - Nombre d'établissement par secteurs  OK 
+# - Nombre d'établissement par arrondissement OK 
+# - Nombre d'établissement par secteur + arrondissement OK 
+# - Duré de vie (moyenne) 
+# - Evolution du nombre d'établissement ces 10 dernières années %
+# - Ancienneté des établissement toujours ouverte 
+# - Proportion des établissment selon leurs classification (ex : PME ect..)
+
+
+
+
 from sqlalchemy import create_engine
 import pandas as pd
 import pymysql
@@ -23,17 +34,25 @@ def start() :
     a = "0"
     running = True
     while running == True :
-        a = input("Bienvenue!\n\nTapez 1 pour acceder au premier choix.\nTapez 2 pour acceder au second choix.\nTapez 3 pour acceder au troisième choix.\nTapez 4 pour acceder au quatrième choix.\nTapez 5 pour quitter.")
-        if a == "1" :
-            print("vous venez de choisir le premier choix.")
-            df = pd.read_sql("SELECT * FROM alias_etab_marseille",con=engine)
+        a = input("Bienvenue!\n\nTapez 1 pour acceder au premier choix.\nTapez 2 pour acceder au second choix.\nTapez 3 pour acceder au troisième choix.\nTapez 4 pour acceder au quatrième choix.\nTapez 5 pour quitter.\n")
+        if a == "1" :   
+            print("Vous venez de choisir le premier choix.\n")
+            y = input("Selectionner un secteur\n")
+            df = pd.read_sql('SELECT COUNT(*) FROM alias_etab_marseille WHERE LIB_NAP600 = "%s"' % (y) ,engine)
             print(df)
         elif a == "2" :
-            print("vous venez de choisir le second choix.")
+            print("vous venez de choisir le second choix.\n")
+            y = input("Selectionner un arrondissement\n")
+            df = pd.read_sql('SELECT COUNT(*) FROM alias_etab_marseille WHERE codePostalEtablissement = "%s"' % (y) ,engine)
+            print(df)
         elif a == "3" :
-            print("vous venez de choisir le troisième choix.")
+            print("vous venez de choisir le troisième choix.\n")
+            n = input("Selectionner arrondissement\n")
+            y = input("Selectionner un secteur\n")
+            df = pd.read_sql('SELECT COUNT(*) FROM alias_etab_marseille WHERE codePostalEtablissement = "%s" AND "%s"' % (y,n),engine)
+            print(df)
         elif a == "4" :
-            print("vous venez de choisir le quatrième choix.")
+            print("vous venez de choisir le quatrième choix.\n")
         elif a == "5" :
             quitter = "Y" or "y" 
             if quitter == "Y" or quitter == "y":
